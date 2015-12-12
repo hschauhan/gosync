900f6da85ebb72f5ba9ce00711649f7279ae4173
Himanshu Chauhan <hschauhan@nulltrace.org>
2015-12-12 11:14:00 +0530

    Miscellaneous changes:
    Rate limit the queries.
    Few cosmetic changes.
    Calculate the total number of files available before
    calculating the total categorical usage.
    
    Signed-off-by: Himanshu Chauhan <hschauhan@nulltrace.org>

--- GoSyncModel.py
+++ GoSyncModel.py
@@ -34,11 +34,24 @@ class MD5ChecksumCalculationFailed(RuntimeError):
     """Calculation of MD5 checksum on a given file failed"""
 class RegularFileUploadFailed(RuntimeError):
     """Upload of a regular file failed"""
+class FileListQueryFailed(RuntimeError):
+    """The query of file list failed"""
 
 audio_file_mimelist = ['audio/mpeg', 'audio/x-mpeg-3', 'audio/mpeg3', 'audio/aiff', 'audio/x-aiff']
 movie_file_mimelist = ['video/mp4', 'video/x-msvideo', 'video/mpeg', 'video/flv', 'video/quicktime']
 image_file_mimelist = ['image/png', 'image/jpeg', 'image/jpg', 'image/tiff']
-document_file_mimelist = ['application/powerpoint', 'applciation/mspowerpoint', 'application/x-mspowerpoint', 'application/pdf', 'application/x-dvi']
+document_file_mimelist = ['application/powerpoint', 'applciation/mspowerpoint', \
+                              'application/x-mspowerpoint', 'application/pdf', \
+                              'application/x-dvi']
+google_docs_mimelist = ['application/vnd.google-apps.spreadsheet', \
+                            'application/vnd.google-apps.sites', \
+                            'application/vnd.google-apps.script', \
+                            'application/vnd.google-apps.presentation', \
+                            'application/vnd.google-apps.fusiontable', \
+                            'application/vnd.google-apps.form', \
+                            'application/vnd.google-apps.drawing', \
+                            'application/vnd.google-apps.document', \
+                            'application/vnd.google-apps.map']
 
 class GoSyncModel(object):
     def __init__(self):
@@ -48,6 +61,7 @@ class GoSyncModel(object):
         self.drivePhotoUsage = 0
         self.driveDocumentUsage = 0
         self.driveOthersUsage = 0
+        self.totalFilesToCheck = 0
 
         self.config_path = expanduser("~") + "/.gosync"
         self.credential_file = self.config_path + "/.credentials.json"
@@ -134,7 +148,6 @@ class GoSyncModel(object):
                 print "Found!\n"
                 return f
 
-        print "Not found\n"
         return None
 
     def LocateFolderOnDrive(self, folder_path):
@@ -144,51 +157,53 @@ class GoSyncModel(object):
         if the path walking fails at any stage.
         """
         dir_list = folder_path.split(os.sep)
-        print dir_list
         croot = 'root'
         for dir1 in dir_list:
-            print dir1
-            folder = self.GetFolderOnDrive(dir1, croot)
-            if not folder:
-                raise FolderNotFound()
+            try:
+                folder = self.GetFolderOnDrive(dir1, croot)
+                if not folder:
+                    raise FolderNotFound()
+            except:
+                raise
 
             croot = folder['id']
 
         return folder
 
     def LocateFileInFolder(self, filename, parent='root'):
-        file_list = self.drive.ListFile({'q': "'%s' in parents and trashed=false" % parent}).GetList()
-        for f in file_list:
-            if f['title'] == filename:
-                print "found file %s in %s\n" % (filename, parent)
-                return f
+        try:
+            file_list = self.MakeFileListQuery({'q': "'%s' in parents and trashed=false" % parent})
+            for f in file_list:
+                if f['title'] == filename:
+                    return f
 
-        raise FileNotFound()
+            raise FileNotFound()
+        except:
+            raise
+
 
     def LocateFileOnDrive(self, abs_filepath):
         dirpath = os.path.dirname(abs_filepath)
         filename = self.PathLeaf(abs_filepath)
-        print "LocateFileOnDrive: dirpath %s filename %s\n" % (dirpath, filename)
         try:
             f = self.LocateFolderOnDrive(dirpath)
-            print "LocateFileOnDrive: Folder %s found\n" % dirpath
         except FolderNotFound:
-            print "LocateFileOnDrive: Folder %s not found\n" % dirpath
+            raise
+        except FileListQueryFailed:
             raise
 
         try:
             fil = self.LocateFileInFolder(filename, f['id'])
-            print "LocateFileOnDrive: File %s found on %s\n" % (filename, f['id'])
             return fil
         except FileNotFound:
-            print "LocateFileOnDrive: File %s not found\n" % filename
+            raise
+        except FileListQueryFailed:
             raise
 
     def CreateDirectoryInParent(self, dirname, parent_id='root'):
         upfile = self.drive.CreateFile({'title': dirname,
                                         'mimeType': "application/vnd.google-apps.folder",
                                         "parents": [{"kind": "drive#fileLink", "id": parent_id}]})
-        print "Creating directory by name %s\n" % dirname
         upfile.Upload()
 
     def CreateDirectoryByPath(self, dirpath):
@@ -197,12 +212,9 @@ class GoSyncModel(object):
         dirname = self.PathLeaf(dirpath)
 
         try:
-            print "Locating %s on Drive\n" % drivepath
             f = self.LocateFolderOnDrive(drivepath)
-            print "Folder %s already exists on Drive\n" % dirpath
             return
         except FolderNotFound:
-            print "New folder %s\n" % dirpath
             if basepath == '':
                 self.CreateDirectoryInParent(dirname)
             else:
@@ -215,32 +227,32 @@ class GoSyncModel(object):
                                             wx.ID_OK | wx.ICON_EXCLAMATION)
                     dial.ShowModal()
                     return
+        except FileListQueryFailed:
+            errorMsg = "Server Query Failed!\n"
+            dial = wx.MessageDialog(None, errorMsg, 'Directory Not Found',
+                                    wx.ID_OK | wx.ICON_EXCLAMATION)
+            dial.ShowModal()
+            return
 
     def CreateRegularFile(self, file_path, parent='root', uploaded=False):
         filename = self.PathLeaf(file_path)
         upfile = self.drive.CreateFile({'title': filename,
                                        "parents": [{"kind": "drive#fileLink", "id": parent}]})
         upfile.SetContentFile(file_path)
-        print "uploading file %s\n" % file_path
         upfile.Upload()
 
     def UploadFile(self, file_path):
         self.sync_lock.acquire()
         if os.path.isfile(file_path):
             drivepath = file_path.split(self.mirror_directory+'/')[1]
-            print "Uploadfile drive path: %s\n" % drivepath
             try:
                 f = self.LocateFileOnDrive(drivepath)
                 newfile = False
                 if f['md5Checksum'] == self.HashOfFile(file_path):
-                    print "File found on drive and is same.\n"
                     self.sync_lock.release()
                     return
-                else:
-                    print "File found on drive but hash don't match. modified?\n"
             except (FileNotFound, FolderNotFound):
                 newfile = True
-                print "File not found on drive new file.\n"
 
             dirpath = os.path.dirname(drivepath)
             if dirpath == '':
@@ -248,7 +260,6 @@ class GoSyncModel(object):
             else:
                 try:
                     f = self.LocateFolderOnDrive(dirpath)
-                    print "Found folder %s id %s\n" % (f['title'], f['id'])
                     self.CreateRegularFile(file_path, f['id'], newfile)
                 except FolderNotFound:
                     # We are coming from premise that upload comes as part
@@ -263,32 +274,39 @@ class GoSyncModel(object):
         self.sync_lock.release()
 
     ####### DOWNLOAD SECTION #######
+    def MakeFileListQuery(self, query):
+        # Retry 5 times to get the query
+        for n in range (0, 5):
+            try:
+                return self.drive.ListFile(query).GetList()
+            except HttpError as error:
+                if error.resp.reason in ['userRateLimitExceeded', 'quotaExceeded']:
+                    time.sleep((2**n) + random.random())
+
+        raise FileListQueryFailed
+
     def TotalFilesInFolder(self, parent='root'):
         file_count = 0
-        file_list = self.drive.ListFile({'q': "'%s' in parents and trashed=false" % parent}).GetList()
-        for f in file_list:
-            if f['mimeType'] == 'application/vnd.google-apps.folder':
-                file_count += self.TotalFilesInFolder(f['id'])
-            else:
-                file_count += 1
+        try:
+            file_list = self.MakeFileListQuery({'q': "'%s' in parents and trashed=false" % parent})
+            for f in file_list:
+                if f['mimeType'] == 'application/vnd.google-apps.folder':
+                    file_count += self.TotalFilesInFolder(f['id'])
+                else:
+                    file_count += 1
 
-        return file_count
+            return file_count
+        except:
+            raise
 
-    def IsGoogleDocument(self, file_obj):
-        if file_obj['mimeType'] == 'application/vnd.google-apps.spreadsheet' \
-                or file_obj['mimeType'] == 'application/vnd.google-apps.sites' \
-                or file_obj['mimeType'] == 'application/vnd.google-apps.script' \
-                or file_obj['mimeType'] == 'application/vnd.google-apps.presentation' \
-                or file_obj['mimeType'] == 'application/vnd.google-apps.fusiontable' \
-                or file_obj['mimeType'] == 'application/vnd.google-apps.form' \
-                or file_obj['mimeType'] == 'application/vnd.google-apps.drawing' \
-                or file_obj['mimeType'] == 'application/vnd.google-apps.document':
+    def IsGoogleDocument(self, f):
+        if any(f['mimeType'] in s for s in google_docs_mimelist):
             return True
         else:
             return False
 
     def TotalFilesInDrive(self):
-        return TotalFilesInFolder()
+        return self.TotalFilesInFolder()
 
     def DownloadFileByObject(self, file_obj, download_path):
         dfile = self.drive.CreateFile({'id': file_obj['id']})
@@ -298,26 +316,28 @@ class GoSyncModel(object):
             if self.HashOfFile(abs_filepath) == file_obj['md5Checksum']:
                 return
         else:
-            print "Downloading file %s\n" % abs_filepath
             dfile.GetContentFile(abs_filepath)
 
     def SyncDirectory(self, parent, pwd):
         if self.cancelRunningSync:
             return
 
-        file_list = self.drive.ListFile({'q': "'%s' in parents and trashed=false" % parent}).GetList()
-        for f in file_list:
-            if self.cancelRunningSync:
-                return
+        try:
+            file_list = self.MakeFileListQuery({'q': "'%s' in parents and trashed=false" % parent})
+            for f in file_list:
+                if self.cancelRunningSync:
+                    return
 
-            if f['mimeType'] == 'application/vnd.google-apps.folder':
-                abs_dirpath = os.path.join(self.mirror_directory, pwd, f['title'])
-                if not os.path.exists(abs_dirpath):
-                    os.makedirs(abs_dirpath)
-                self.SyncDirectory(f['id'], os.path.join(pwd, f['title']))
-            else:
-                if not self.IsGoogleDocument(f):
-                    self.DownloadFileByObject(f, os.path.join(self.mirror_directory, pwd))
+                if f['mimeType'] == 'application/vnd.google-apps.folder':
+                    abs_dirpath = os.path.join(self.mirror_directory, pwd, f['title'])
+                    if not os.path.exists(abs_dirpath):
+                        os.makedirs(abs_dirpath)
+                        self.SyncDirectory(f['id'], os.path.join(pwd, f['title']))
+                else:
+                    if not self.IsGoogleDocument(f):
+                        self.DownloadFileByObject(f, os.path.join(self.mirror_directory, pwd))
+        except:
+            raise
 
     def run(self):
         while True:
@@ -336,36 +356,51 @@ class GoSyncModel(object):
             return 0
 
     def calculateUsageOfFolder(self, folder_id):
-        file_list = self.drive.ListFile({'q': "'%s' in parents and trashed=false" % folder_id}).GetList()
-        for f in file_list:
-            if f['mimeType'] == 'application/vnd.google-apps.folder':
-                self.calculateUsageOfFolder(f['id'])
-            else:
-                if not self.IsGoogleDocument(f):
-                    if any(f['mimeType'] in s for s in audio_file_mimelist):
-                        self.driveAudioUsage += self.GetFileSize(f)
-                    elif  any(f['mimeType'] in s for s in image_file_mimelist):
-                        self.drivePhotoUsage += self.GetFileSize(f)
-                    elif any(f['mimeType'] in s for s in movie_file_mimelist):
-                        self.driveMoviesUsage += self.GetFileSize(f)
-                    elif any(f['mimeType'] in s for s in document_file_mimelist):
-                        self.driveDocumentUsage += self.GetFileSize(f)
-                    else:
-                        self.driveOthersUsage += self.GetFileSize(f)
+        try:
+            file_list = self.MakeFileListQuery({'q': "'%s' in parents and trashed=false" % folder_id})
+            for f in file_list:
+                if f['mimeType'] == 'application/vnd.google-apps.folder':
+                    self.calculateUsageOfFolder(f['id'])
+                else:
+                    if not self.IsGoogleDocument(f):
+                        if any(f['mimeType'] in s for s in audio_file_mimelist):
+                            self.driveAudioUsage += self.GetFileSize(f)
+                        elif  any(f['mimeType'] in s for s in image_file_mimelist):
+                            self.drivePhotoUsage += self.GetFileSize(f)
+                        elif any(f['mimeType'] in s for s in movie_file_mimelist):
+                            self.driveMoviesUsage += self.GetFileSize(f)
+                        elif any(f['mimeType'] in s for s in document_file_mimelist):
+                            self.driveDocumentUsage += self.GetFileSize(f)
+                        else:
+                            self.driveOthersUsage += self.GetFileSize(f)
+        except:
+            raise
 
     def calculateUsage(self):
         while True:
             self.sync_lock.acquire()
-            print "calculating drive usage\n"
             self.calculatingDriveUsage = True
             self.driveAudioUsage = 0
             self.driveMoviesUsage = 0
             self.driveDocumentUsage = 0
             self.drivePhotoUsage = 0
             self.driveOthersUsage = 0
-            self.calculateUsageOfFolder('root')
+            try:
+                self.totalFilesToCheck = self.TotalFilesInDrive()
+                print "Total files to check %d\n" % self.totalFilesToCheck
+            except:
+                print "failed to get the total files in drive\n"
+
+            try:
+                self.calculateUsageOfFolder('root')
+            except:
+                self.driveAudioUsage = 0
+                self.driveMoviesUsage = 0
+                self.driveDocumentUsage = 0
+                self.drivePhotoUsage = 0
+                self.driveOthersUsage = 0
+
             self.calculatingDriveUsage = False
-            print "calculating drive usage done\n"
             self.sync_lock.release()
             time.sleep(300)
 
